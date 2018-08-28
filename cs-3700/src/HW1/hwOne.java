e436f2bfcd8a3c07391dbada8764b67e3c133850
Jeremy Canning <jscanning@cpp.edu>
2018-08-28 10:38:48 -0700

    commit three
    HW1 finished

--- cs-3700/src/HW1/hwOne.java
+++ cs-3700/src/HW1/hwOne.java
@@ -4,7 +4,7 @@ public class hwOne {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello, my name is Jeremy Canning.");
+		System.out.println("Goodbye, I finished my CS 3700 HW1.");
 	}
 
 }
