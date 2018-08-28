3321a1fd5023af191b264148d9499118eb5167eb
Jeremy Canning <jscanning@cpp.edu>
2018-08-28 10:35:35 -0700

    second commit

--- cs-3700/src/HW1/hwOne.java
+++ cs-3700/src/HW1/hwOne.java
@@ -2,9 +2,9 @@ package HW1;
 
 public class hwOne {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello, my name is Jeremy Canning.");
 	}
 
 }
