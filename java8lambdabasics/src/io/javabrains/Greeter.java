7689d40f60a96ac91f2449328145371f0055a5b4
Jeremy Canning <jscanning@cpp.edu>
2018-09-30 20:23:49 -0700

    4th commit-1st Lambda Function

---
+++ java8lambdabasics/src/io/javabrains/Greeter.java
@@ -0,0 +1,20 @@
+package io.javabrains;
+
+public class Greeter {
+
+	public void greet(Greeting greeting){
+		greeting.perform();
+	}
+	
+	public static void main(String[] args) {
+		Greeter greeter = new Greeter();
+		Greeting helloWorldGreeting = new HelloWorldGreeting();
+		greeter.greet(helloWorldGreeting);
+		
+		Greeting myLambdaFunction = () -> System.out.print("Hello World from Lambda!");
+		
+		greeter.greet(myLambdaFunction);
+		
+	}
+
+}
