ac20a8c9ba3eaf4c8033be24a1c1310e540e966a
jscanning <jscanning@cs-851-D8SRCP2.win.csupomona.edu>
2018-10-01 18:41:11 -0700

    commit hw2 Lecture9 code examples

---
+++ cs-3700/src/homework/hw2/Lecture9/Barrier.java
@@ -0,0 +1,34 @@
+package homework.hw2.Lecture9;
+
+public class Barrier 
+
+{ 
+
+    public synchronized void block() throws InterruptedException 
+
+    { 
+
+        wait(); 
+
+    } 
+
+
+    public synchronized void release() throws InterruptedException 
+
+    { 
+
+        notify(); 
+
+    } 
+
+
+    public synchronized void releaseAll() throws InterruptedException 
+
+    { 
+
+        notifyAll(); 
+
+    } 
+
+
+} 
