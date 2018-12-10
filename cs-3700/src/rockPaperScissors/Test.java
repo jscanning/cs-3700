95dd23e173cb00bd10a122a68627733c74f2d565
Jeremy Canning <jscanning@cpp.edu>
2018-12-09 21:30:55 -0800

    CS3700 Final Project Commit 4 - Getting unknown compilation error
    Unsure how to proceed and complete this.
    
    
    Signed-off-by: Jeremy Canning <jscanning@cpp.edu>

--- cs-3700/src/rockPaperScissors/Test.java
+++ cs-3700/src/rockPaperScissors/Test.java
@@ -1,38 +1,53 @@
 package rockPaperScissors;
 
+import java.awt.List;
+import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Test {
-
+	public static String programName = "/cs-3700/src/rockPaperScissors/Player.java";
+	public static String arg0 = "228.5.6.7";
+	public static String arg1 = "6789";
+	
 	public static void main(String[] args) {
-		try {
-			Runtime.getRuntime().exec("javac C:/Users/Jeremy Canning/git/cs-3700/cs-3700/src/rockPaperScissors/Player.java");
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		String programName = "/cs-3700/src/rockPaperScissors/Player.java";
-		String arg0 = "228.5.6.7";
-		String arg1 = "6789";
+		
+		String workingDir = System.getProperty("user.dir");
+		System.out.println("Working dir: " + System.getProperty("user.dir"));
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Enter number of games to play: ");
 		String arg2 = String.valueOf(kb.nextInt());
-		String[] command = {"java.exe", "-cp", "bin", "rockPaperScissors.Player"};
-		ProcessBuilder pb = new ProcessBuilder(command);
+		String[] exec_cmd = {"java", "-cp", "bin", "Player", arg0, arg1, arg2};
+
 		try {
-				Process[] players = {pb.inheritIO().start(), pb.start(), pb.start()};
-				for(Process player : players) {
-					player.waitFor();
-				}
+			String[] cmd = {"javac.exe", "Player.java"};
+			ProcessBuilder compile = new ProcessBuilder(cmd);
+			compile.directory(new File(workingDir));
+			//System.out.println(compile.directory().getAbsolutePath());
+			Process p = compile.start();
+			p.waitFor();
+			p.destroy();
 		} catch (IOException | InterruptedException e) {
 			System.out.println("Error");
 			e.printStackTrace();
+		}finally{
+			try{
+			ProcessBuilder pb = new ProcessBuilder(exec_cmd);
+			pb.directory(new File(workingDir));
+			pb.inheritIO();
+			System.out.println(Arrays.toString(pb.command().toArray()));
+			//System.out.println(pb.directory().getAbsolutePath());
+			Process[] players = {pb.start(), pb.start(), pb.start()};
+			for(Process player : players) {
+				player.waitFor();
+			}
+			}catch(IOException | InterruptedException e){
+				e.printStackTrace();
+			}
 		}
+		kb.close();
 	}
 
 }
